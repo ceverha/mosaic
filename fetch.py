320d7509a100cb9c39f1f2a9663a96fb36c5766c
EC2 Default User <ec2-user@ip-172-31-47-234.us-west-2.compute.internal>
2019-04-21 22:50:52 +0000

    ready to experiment, trying to add extra data storage

--- fetch.py
+++ fetch.py
@@ -11,15 +11,18 @@ from subtitle_indexing import SubtitleIndex
 import gensim
 import gensim.downloader as api
 
+from pathlib import Path
+
 # nltk.download('stopwords')
 from nltk.corpus import stopwords
 english_stopwords= set(stopwords.words('english'))
 
 class MosaicPlay:
-    def __init__(self):
+    def __init__(self, char_names=['A','B']):
         self.name = ""
         self.lines = []
-        self.char_names = ['JACK', 'JILL']
+        self.sim_pairs = []
+        self.char_names = char_names
         self.curr_char_index = 0
 
     def next_char(self):
@@ -39,17 +42,21 @@ class MosaicPlay:
     def get_lines(self):
         return self.lines
 
+    def add_sim_pair(self, left, right):
+        self.sim_pairs.append([left, right])
+
     def get_length(self):
         return len(self.lines)
 
 class MosaicConstructor:
-    def __init__(self):
-        self.play = MosaicPlay()
-        
+    def new_play(self, characters):
+        self.play = MosaicPlay(char_names=characters)
+
     def load_model(self, model_string):
         self.model = api.load(model_string)
     
     def load_narrative(self, path_to_narrative):
+        self.narrative_path = path_to_narrative
         with open(path_to_narrative, "r") as f:
             text = f.read()
             narrative = text.split("\n")
@@ -100,7 +107,7 @@ class MosaicConstructor:
         else:
             similar_chunks = self.index.get_all_chunks()
         
-        print("Found %d valid chunks, computing similarity now..." % len(similar_chunks))        
+        # print("Found %d valid chunks, computing similarity now..." % len(similar_chunks))        
         if len(similar_chunks) < 1:
             return oov_chunk
 
@@ -118,6 +125,7 @@ class MosaicConstructor:
                     continue
                 sim = self.model.n_similarity(in_vocab_source, in_vocab_candidate)
                 candidate_ids[chunk_id] = sim
+
         
         # get offset chunks if necessary
         offset_chunks = {}
@@ -143,14 +151,18 @@ class MosaicConstructor:
 
     # defaults to 1 candidate with two unsupervised replies
     def generate(self, num_candidates=1, offset=1, pattern_string="0,0", use_index=True):
-        output_pattern = list(map(int, pattern_string.split(",")))
+        # for use in saving the play
+        narrative_name = os.path.basename(self.narrative_path).strip(".txt")
+        self.output_id = f"{narrative_name}_{pattern_string}_{use_index}_{offset}"
+
+        output_pattern = list(map(int, pattern_string.split("-")))
         for narrative_line in self.narrative:
             source_chunk = narrative_line
             print("\n%s:\n\t%s" % (self.play.curr_char(), source_chunk))
             self.play.add_line(source_chunk)
             for output_type in output_pattern:
                 if output_type == 0:
-                    similar_chunks = self.get_similar_chunks(source_chunk, num_candidates, remove_stopwords=True, offset=offset, use_index=use_index)
+                    similar_chunks = self.get_similar_chunks(source_chunk, num_candidates, remove_stopwords=False, offset=offset, use_index=use_index)
                     if num_candidates > 1:
                         for i, candidate in enumerate(similar_chunks):
                             if i > num_candidates:
@@ -165,44 +177,71 @@ class MosaicConstructor:
                     source_chunk = input("\n%s:\n\t" % self.play.curr_char())
                 self.play.add_line(source_chunk)
 
-    def print_play(self):
-        var.format(line[0], line[1])
-        for line in self.play.get_lines():
-            print("\n%s:\n\t%s" % (line[0], line[1]))
+    # def print_play(self):
+    #     var.format(line[0], line[1])
+    #     for line in self.play.get_lines():
+    #         print("\n%s:\n\t%s" % (line[0], line[1]))
 
-    def save_play(self, file_path):
-        with open(file_path, "w") as f:
+    def save_play(self, folder_path):
+        path = f"{folder_path}/{self.output_id}"
+        with open(path, "w") as f:
             for line in self.play.get_lines():
                 f.write("\n%s:\n\t%s\n" % (line[0], line[1]))
-
-if __name__ == "__main__":
+            print(f"Saved play at {path}")
+        extra_path = f"{folder_path}/{self.output_id}-extra"
+        with open(extra_path, "w") as f:
+            for line in self.play.get_lines():
+                f.write("\n%s:\n\t%s\n" % (line[0], line[1]))
+            print(f"Saved play at {path}")
+if __name__ == "__main__":    
+    
     mosaic = MosaicConstructor()
 
-    print("loading narrative: %s" % sys.argv[2])
-    mosaic.load_narrative(sys.argv[2])
+    print("loading index: %s" % sys.argv[1])
+    mosaic.load_index(sys.argv[1])
 
-    print("loading index: %s" % sys.argv[3])
-    mosaic.load_index(sys.argv[3])
+    narratives_folder = sys.argv[3]
+    if not Path(narratives_folder).is_dir():
+        print(f"invalid folder: {narratives_folder}")
 
-    print("loading model: %s" % sys.argv[1])
-    mosaic.load_model(sys.argv[1])
-    
-    pattern_string = sys.argv[4]
+    output_folder = sys.argv[4]
+    if not Path(output_folder).is_dir():
+        print(f"invalid folder: {output_folder}")
 
-    use_index = sys.argv[5]
-
-    print("starting generation")
-    num_candidates = 1
-    # offset of 1 means the system will select the reply to the most similar piece of dialogue
-    offset = 1
-    
+    # pattern_string = input("Pattern string eg. 0-0 or 1-0: ")
+    pattern_string = sys.argv[5]
+    # offset = int(input("Offset for reply: "))
+    offset = int(sys.argv[6])
+    # use_index = input("Use index? yes or no: ")
+    use_index = sys.argv[7]
     if use_index == "yes":
         use_index = True
     else:
         use_index = False
+    # characters
+    characters = sys.argv[8].split("-")
 
-    mosaic.generate(num_candidates=num_candidates, offset=offset, pattern_string=pattern_string, use_index=use_index)
+    print("loading model: %s" % sys.argv[2])
+    mosaic.load_model(sys.argv[2])
+    
+    while True:
+        for narrative in os.listdir(narratives_folder):
+            mosaic.new_play(characters)
+            print(f"Current narrative: {narrative}")
+            mosaic.load_narrative(f"{narratives_folder}/{narrative}")
+            print("\nPLAY START\n")
+            mosaic.generate(num_candidates=1, offset=offset, pattern_string=pattern_string, use_index=use_index)
+            print("\nPLAY END\n")
+            mosaic.save_play(output_folder)
 
-    # mosaic.print_play()
-
-    mosaic.save_play(sys.argv[6])
+        decision = input("Go again with different parameters?")
+        if decision == 'yes':
+            pattern_string = input("Pattern string eg. 0-0 or 1-0: ")
+            offset = int(input("Offset for reply: "))
+            use_index = input("Use index? yes or no: ")
+            if use_index == "yes":
+                use_index = True
+            else:
+                use_index = False
+        else:
+            break
