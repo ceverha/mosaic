aa5b6883d512c4f3f1409789746cf4c48e10fdcc
EC2 Default User <ec2-user@ip-172-31-47-234.us-west-2.compute.internal>
2019-04-24 17:16:28 +0000

    all experiment data

--- pair_generate.sh
+++ pair_generate.sh
@@ -6,6 +6,6 @@ folder_path=experiment_output/$1-$(timestamp)
 mkdir $folder_path
 echo $folder_path
 
-python fetch.py play_dialogue_index.obj glove-wiki-gigaword-100 experiment_narratives $folder_path 1-1 1 yes A-B-C
-python fetch.py play_dialogue_index.obj glove-wiki-gigaword-100 experiment_narratives $folder_path 1-1 1 no A-B-C
-python fetch.py subtitles_100.obj glove-wiki-gigaword-100 experiment_narratives $folder_path 1-1 1 no A-B-C
+python fetch.py play_dialogue_index.obj glove-wiki-gigaword-100 experiment_narratives $folder_path 1-1-0-1-1 1 yes A-B-C
+# python fetch.py play_dialogue_index.obj glove-wiki-gigaword-100 experiment_narratives $folder_path 1-1 1 no A-B-C
+# python fetch.py subtitles_100.obj glove-wiki-gigaword-100 experiment_narratives $folder_path 1-1 1 no A-B-C
--- solo_generate.sh
+++ solo_generate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+clear;
 timestamp() {
   date +"%m-%d_%H:%M"
 }
@@ -7,5 +8,5 @@ mkdir $folder_path
 echo $folder_path
 
 python fetch.py play_dialogue_index.obj glove-wiki-gigaword-100 experiment_narratives $folder_path 1-0-1 1 yes A-B
-python fetch.py play_dialogue_index.obj glove-wiki-gigaword-100 experiment_narratives $folder_path 1-0-1 1 no A-B
-python fetch.py subtitles_200.obj glove-wiki-gigaword-100 experiment_narratives $folder_path 1-0-1 1 no A-B
+# python fetch.py play_dialogue_index.obj glove-wiki-gigaword-100 experiment_narratives $folder_path 1-0-1 1 no A-B
+# python fetch.py subtitles_200.obj glove-wiki-gigaword-100 experiment_narratives $folder_path 1-0-1 1 no A-B
