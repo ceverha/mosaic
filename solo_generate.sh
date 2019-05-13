90e8fbb23ddf09898f2b7957a7d22b828ff98f24
EC2 Default User <ec2-user@ip-172-31-47-234.us-west-2.compute.internal>
2019-05-13 12:42:07 +0000

    fixed solo gen

--- solo_generate.sh
+++ solo_generate.sh
@@ -7,6 +7,15 @@ folder_path=experiment_output/$1-$(timestamp)
 mkdir $folder_path
 echo $folder_path
 
-python fetch.py play_dialogue_index.obj glove-wiki-gigaword-100 experiment_narratives $folder_path 1-0-1 1 yes A-B
+# fix this, consult fetch.py
+
+for i in 0 1 2 3 4 5 6 7 8 9
+do
+    sub=$folder_path/$i
+    echo $sub
+    mkdir $sub
+done
+
+python fetch.py play_dialogue_index.obj glove-wiki-gigaword-50 experiment_narratives $folder_path 1-0-1 1 yes A-B
 # python fetch.py play_dialogue_index.obj glove-wiki-gigaword-100 experiment_narratives $folder_path 1-0-1 1 no A-B
 # python fetch.py subtitles_200.obj glove-wiki-gigaword-100 experiment_narratives $folder_path 1-0-1 1 no A-B
